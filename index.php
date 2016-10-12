71425e1debd703d1f9af2dd329190cc85cc431f2
Anthony LAULOM <alaulom@e2r8p16.42.fr>
2016-10-12 16:33:39 +0200

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Camagru</title>
+		<link rel="stylesheet" type="text/css" href="css/general.css">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+	</head>
+	<body>
+		<header>
+			<div id="logo">
+				<h1><a href="index.php">Camagru</a></h1>
+			</div>
+
+		</header>
+
+	</body>
+</html>
