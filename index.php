f5057e86818b35a77a1161e3201ccd96f8f1a936
Anthony LAULOM <alaulom@e2r9p16.42.fr>
2016-10-13 12:36:04 +0200

    integration pdo

--- index.php
+++ index.php
@@ -6,13 +6,34 @@
 		<link rel="stylesheet" type="text/css" href="css/general.css">
 		<link rel="stylesheet" type="text/css" href="css/index.css">
 	</head>
+	<style>
+		.img-nail {
+			width: 100%;
+			float: left;
+		}
+
+		@media (min-width: 760px) {
+			.img-nail {
+				width: 30%;
+			}
+		}
+	</style>
 	<body>
 		<header>
 			<div id="logo">
 				<h1><a href="index.php">Camagru</a></h1>
 			</div>
-
 		</header>
-
+		<div id="log">
+			<input type="button" type="submit" name="connexion" value="Se connecter">
+			<input type="button" type="submit" name="register" value="S'incrire">
+		</div>
+	<div class="gallery" style="width: 100%">
+		<?php for ($i = 0; $i < 12; $i++): ?>
+			<div class="img-nail">
+				<img src="https://d1ra4hr810e003.cloudfront.net/media/27FB7F0C-9885-42A6-9E0C19C35242B5AC/0/D968A2D0-35B8-41C6-A94A0C5C5FCA0725/F0E9E3EC-8F99-4ED8-A40DADEAF7A011A5/dbe669e9-40be-51c9-a9a0-001b0e022be7/thul-IMG_2100.jpg" alt="cat" style="width: 100%; height: auto;">
+			</div>
+		<?php endfor;?>
+	</div>
 	</body>
 </html>
