c2a8b61eadda9c298fb24d52c50d47ad46047d35
Anthony LAULOM <anthonylaulom@gmail.com>
2016-11-10 15:03:10 +0100

    objet user

--- objet/user.php
+++ objet/user.php
@@ -17,4 +17,77 @@ class user
     private $is_activ,
     private $hash,
     private $salt;
+
+//
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+//
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+//
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+//
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+//
+    public function getIsAdmin()
+    {
+        return $this->is_admin;
+    }
+    public function setIsAdmin($is_admin)
+    {
+        $this->is_admin = $is_admin;
+    }
+//
+    public function getIsActiv()
+    {
+        return $this->is_activ;
+    }
+    public function setIsActiv($is_activ)
+    {
+        $this->is_activ = $is_activ;
+    }
+//
+    public function getHash()
+    {
+        return $this->hash;
+    }
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+    }
+//
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
 }
