adf70712a9182280d1557aa8aa1ca4d70df33874
Anthony LAULOM <anthonylaulom@gmail.com>
2016-11-16 17:12:43 +0100

    modif css/html

--- sign-up.php
+++ sign-up.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<html>
 <head>
     <meta charset="utf-8">
     <title>sign-up</title>
     <link rel="stylesheet" type="text/css" href="css/sign-up.css">
 </head>
 <body>
-<header>
-    <div id="logo">
-        <h1 href="sign-up.php">Inscription</h1><br/>
-        <p class="logo-msg">Postes tes photos dégueulasses</p>
-    </div>
-</header>
 <div class="container">
-    <form id="signupForm" method="post" action="index.php">
-        <div id="register">
-            <label class="col-xs-12">Email</label><br/>
-            <input type="text" name="email" placeholder="email"><br/>
-            <label class="col-xs-12">Utilisateur</label><br/>
-            <input type="text" name="login" placeholder="utilisateur"><br/>
-            <label class="col-xs-12">Mot de passe</label><br/>
-            <input type="password" name="password" placeholder="mot de passe"><br/>
-            <label class="col-xs-12">Confirmer mot de passe</label><br/>
-            <input type="password" name="password-conf" placeholder="mot de passe"><br/>
-            <input type="submit" name="valider" value="Souriez">
+    <div id="register">
+        <div id="logo">
+            <h1 href="sign-up.php">Inscription</h1>
+            <p class="logo-msg">Postes tes photos dégueulasses</p>
         </div>
-    </form>
+        <form id="signupForm" method="post" action="index.php">
+            <div class="box">
+                <input type="text" name="email" placeholder="email">
+            </div>
+            <div class="box">
+                <input type="text" name="login" placeholder="utilisateur">
+            </div>
+            <div class="box">
+                <input type="password" name="password" placeholder="mot de passe">
+            </div>
+            <div class="box">
+                <input type="password" name="password-conf" placeholder="mot de passe">
+            </div>
+            <div class="box">
+                <input type="submit" name="valider" value="Souriez">
+            </div>
+        </form>
+    </div>
 </div>
 </body>
 </html>
