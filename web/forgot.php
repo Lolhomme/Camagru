62073d1d8238167a19438a16cd11883139b43a61
Anthony LAULOM <anthonylaulom@gmail.com>
2016-12-29 16:08:55 +0100

    forgot work

--- web/forgot.php
+++ web/forgot.php
@@ -3,26 +3,48 @@
 require_once ("includes/dbConnect.php");
 
 $errors = array();
-if (!empty($_POST))
-{
-    $email = htmlspecialchars($_POST['email']);
-    $req = $db->prepare("select count (*) from users where email=:email");
-    $req->bindValue(':email', $email);
+if (!empty($_POST)) {
+    $mail = htmlspecialchars($_POST['email']);
+    $req = $db->prepare("select count(*) from users where mail=:mail");
+    $req->bindValue(':mail', $mail);
     $req->execute();
 
-    $req2 = $db->prepare("select active from users where email=:email");
-    $req2->bindValue(':email', $email);
+    $req2 = $db->prepare("select activate from users where mail=:mail");
+    $req2->bindValue(':mail', $mail);
     $req2->execute();
 
     //All errors
     if ($req->fetchColumn() == 0)
         $errors['emailDontExist'] = true;
-    if ($req2->fetchColumn()!= "1");
-        $errors['Compte inactif ou mot de passe déjà reset'] = true;
-}
-    if (empty($errors))
-?>
+    /*if ($req2->fetchColumn() != "1") ;
+        $errors['inactiveAccount'] = true;*/
 
+    $recover = hash("md5", time());
+    $link = ($_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["HTTP_HOST"] . '/recover.php?v=' . $recover);
+
+    //Recovery email
+    $destinataire = $_POST['email'];
+    $subject = 'Mot de passe oublié.';
+    $header = 'De Camagru tête de cul';
+    $message = "Vous recevez ce mail car vous avez oublié votre mot de passe. Si ce n\'est pas le cas, n\'en tenez pas compte.
+    
+    Pour réinitialiser votre mot de passe veuillez cliquer ici :
+    
+    '$link'
+    -----------------------
+     Ceci est un email automatique, veuillez ne pas y répondre.";
+
+    if (empty($errors)) {
+        $req = $db->prepare("update users set confirmKey=:recover where mail=:mail");
+        $req->bindValue(':recover', $recover);
+        $req->bindValue(':mail', $mail);
+        if ($req->execute()) {
+            mail($destinataire, $subject, $header, $message);
+            $success = 'Un email vous a été envoyé.';
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,8 +52,6 @@ if (!empty($_POST))
     <title>Camagru</title>
     <link rel="stylesheet" type="text/css" href="./css/index.css">
 </head>
-<style>
-</style>
 <body>
 <div class="nav">
     <a href="index.php">Camagru</a>
@@ -44,6 +64,14 @@ if (!empty($_POST))
         <form id="ForgotPassword" method="post">
             <div id="log">
                 <div class="box">
+                    <?php
+                    if (isset($errors['emailDontExist']))
+                        echo "<h4>Cet email n'est associé a aucun compte.</h4>";
+                    else if (isset($errors['inactiveAccount']))
+                        echo "<h4>Compte inactif ou mot de passe déjà reset.</h4>";
+                    if (isset($success))
+                        echo "<h4>$success</h4>";
+                    ?>
                     <input type="email" name="email" placeholder="Email">
                 </div>
                 <div class="box">
